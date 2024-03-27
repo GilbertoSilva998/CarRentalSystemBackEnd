7d081e317915fc1ec0198cc2dbdf5ceb99409fe1
sinothando <sinothandomasiki@gmail.com>
2024-03-27 20:31:07 +0200

    Add files via upload

--- src/main/java/za/ac/cput/factory/FeedbackFactory.java
+++ src/main/java/za/ac/cput/factory/FeedbackFactory.java
@@ -1,19 +1,36 @@
 package za.ac.cput.factory;
 //Sinothando Masiki 219153841
 
-import za.ac.cput.domain.Customer;
-import za.ac.cput.domain.Feedback;
 
-import java.util.Date;
+import za.ac.cput.domain.Feedback;
+import za.ac.cput.util.Helper;
+
 
 public class FeedbackFactory {
 
     // Method to create a feedback object
-    public static Feedback createFeedback(String feedbackId, String rental, String customer, String rating, String comments, String feedbackDate) {
+    public static Feedback createFeedback(String rental,
+                                          String customer,
+                                          String rating,
+                                          String comments,
+                                          String feedbackDate) {
+        if (Helper.isNullorEmpty(rental) || Helper.isNullorEmpty(customer)){
+            return null;
+        }
+
+        String feedbackId = Helper.generateId();
+
+        Feedback feedback = new Feedback.Builder().setFeedbackId(feedbackId)
+                .setRental(rental)
+                .setCustomer(customer)
+                .setRating(rating)
+                .setComments(comments)
+                .setFeedbackDate(feedbackDate)
+                .build();
+        return feedback;
 
 
-        // Create and return a new feedback object
-        return new Feedback(feedbackId, rental, customer, rating, comments, feedbackDate);
+
     }
 }
 
